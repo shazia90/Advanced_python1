f246d18bf8b89fcf21bc3b8aa9a2def5b649dd13
shazia90 <61934410+shazia90@users.noreply.github.com>
2020-04-01 23:59:40 +0530

    Add files via upload

---
+++ Day2_Ad_Py_B13.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Numpy introduction:
+
+
+# In[ ]:
+
+
+It is considered as the fundamental package for scientific computing in python: 
+
+
+# In[ ]:
+
+
+wide and high end variety of mathametical operations
+
+
+# In[ ]:
+
+
+we will be working with N dimensional arrays**** which is making super fast in processing
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+array_one = np.array ([1, 2, 3, 4])
+
+array_one
+
+
+# In[3]:
+
+
+type(array_one)
+
+
+# In[4]:
+
+
+numbers=[6, 7, 8, 9]
+
+
+# In[6]:
+
+
+array_two = np.array(numbers)
+
+array_two
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

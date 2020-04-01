032c076da10d6cca477e4b232155b697cd7e87e3
shazia90 <61934410+shazia90@users.noreply.github.com>
2020-04-01 20:51:36 +0530

    Add files via upload

---
+++ Day1_Ad_Py_B13.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Creation of a project--->Select as create new project
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Numpy--->numerical computions---!
+
+
+# In[ ]:
+
+
+Introduction to Numpy:Installation process
+
+
+# In[2]:
+
+
+get_ipython().system('pip install numpy')
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
